2480d02d7b4920a6ec5eb4b8d6fa0effb4c90b69
bitzj2015 <bitzj2015@outlook.com>
2022-02-03 17:12:18 -0800

    init

--- run.sh
+++
@@ -1,5 +0,0 @@
-python main.py --subset 1 --version linear_1 --model fcnn &> ./param/log_linear_1.txt
-python main.py --subset 2 --version linear_2 --model fcnn &> ./param/log_linear_2.txt
-python main.py --subset 5 --version linear_5 --model fcnn &> ./param/log_linear_5.txt
-python main.py --subset 10 --version linear_10 --model fcnn &> ./param/log_linear_10.txt
-python main.py --subset 20 --version linear_20 --model fcnn &> ./param/log_linear_20.txt
